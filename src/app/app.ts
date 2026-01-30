559ff4e3618402ddd258f8dbb7752b608f92fe2c
prakash <prakash@gmail.com>
2026-01-30 14:22:15 +0530

    add admin and dashboard component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Dashboard } from "./components/dashboard/dashboard";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Dashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
